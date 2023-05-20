ebeddc6d9f3c1f69c6591f41fc5eeeff55312b00
Pedro Endres <86131035+PedroEndres@users.noreply.github.com>
2023-05-20 18:10:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let nombre = "Pedro";
+let apellido = "Endres";
+
+console.log(nombre + " " + apellido);
+
+let number1 = 2;
+let number2 = 4;
+let suma = number1 + number2;
+
+console.log(suma);
+
+let suma2 = suma + 6;
+
+console.log(suma2);
